f57d88e2ec2cafe257e46a337f588722669680ce
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-08-24 11:12:02 -0400

    Autocommit por GitHub: alterações: add 'js/data.js'

--- js/data.js
+++ js/data.js
@@ -1,16 +1,9 @@
-// var currentdate = new Date(); 
-// var datetime = "Last Sync: " + currentdate.getDate() + "/"
-//                 + (currentdate.getMonth()+1)  + "/" 
-//                 + currentdate.getFullYear() + " @ "  
-//                 + currentdate.getHours() + ":"  
-//                 + currentdate.getMinutes() + ":" 
-//                 + currentdate.getSeconds();
 let icone, greet;
 
 let interval = setInterval(function() {
     var currentdate = new Date(); 
     var datetime = ('0' + currentdate.getDate()).slice(-2) + "/"
-    + (currentdate.getMonth()+1)  + "/" 
+    + ('0' + currentdate.getMonth()).slice(-2)  + "/" 
     + currentdate.getFullYear() + " @ "  
     + ('0' + currentdate.getHours()).slice(-2) + ":"  
     + ('0' + currentdate.getMinutes()).slice(-2) + ":" 
