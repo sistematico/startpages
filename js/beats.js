e0f8ead4fcd94f97b2647033639385c2a59b3a1b
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-08-24 11:22:01 -0400

    Autocommit por GitHub: alterações: add 'js/beats.js'

--- js/beats.js
+++ js/beats.js
@@ -4,6 +4,6 @@ let beatsinterval = setInterval(function() {
     let m = currentdate.getMinutes();
     let s = currentdate.getSeconds();
     // (UTC+1sec + (UTC+1min * 60) + (UTC+1hr * 3600)) / 86.4 
-    let beats = s + (m * 60) + (h * 3600) / 86.4;
+    let beats = parseInt(s + (m * 60) + (h * 3600) / 86.4);
     document.querySelector('.beats').innerHTML = beats;
 },1000);
