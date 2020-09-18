5695695e4792c4646defdb910adbb128431123e3
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-09-18 07:26:01 -0400

    Autocommit por GitHub: alterações: add 'gaegu/js/search.js'

--- gaegu/js/search.js
+++ gaegu/js/search.js
@@ -5,31 +5,31 @@ let search = 'google';
 function chgAction() {
     if (search === 'ddg') {
         search = 'google';
-        document.searchform.action = "https://www.google.com/search";
+        searchform.action = "https://www.google.com/search";
         input.placeholder = 'Google';
     } else if (search === "google") {
         search = 'github';
-        document.searchform.action = "https://github.com/";
+        searchform.action = "https://github.com/";
         input.placeholder = 'Github';
     } else if (search === 'github') {
         search = 'gitlab';
-        document.searchform.action = "https://gitlab.com/";
+        searchform.action = "https://gitlab.com/";
         input.placeholder = 'Gitlab';
     } else if (search === 'gitlab') {
         search = 'arch';
-        document.searchform.action = 'https://wiki.archlinux.org/index.php/';
+        searchform.action = 'https://wiki.archlinux.org/index.php/';
         input.placeholder = 'ArchWiki';
     } else if (search === 'arch') {
         search = 'wikipedia';
-        document.searchform.action = 'https://en.wikipedia.org/w/index.php';
+        searchform.action = 'https://en.wikipedia.org/w/index.php';
         input.placeholder = 'Wikipedia';
     } else if (search === 'wikipedia') {
         search = 'reddit';
-        document.searchform.action = 'https://reddit.com/search/';
+        searchform.action = 'https://reddit.com/search/';
         input.placeholder = 'Reddit';
     } else {
         search = 'ddg';
-        document.searchform.action = 'https://duckduckgo.com/';
+        searchform.action = 'https://duckduckgo.com/';
         input.placeholder = 'DuckDuckGo';
     }
 
