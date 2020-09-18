9679a72a9a81170c8b281a4e4e1d67483ba6a602
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-09-18 10:15:01 -0400

    Autocommit por GitHub: alterações: add 'gaegu/js/search.js'

--- gaegu/js/search.js
+++ gaegu/js/search.js
@@ -1,71 +1,38 @@
 const input = document.getElementById('search');
 const searchform = document.getElementById('searchform');
+const currentIndex = localStorage.getItem('index');
+let index, provider;
 
-let search = 'google', index = 0, provider;
+if (currentIndex) {
+    index = currentIndex;
+} else {
+    index = 0;
+}
 
 const providers = [
     {name: 'Google', url: 'https://www.google.com/search'},
-    {name: 'DuckDuckGo', url: 'https://duckduckgo.com/'}
+    {name: 'DuckDuckGo', url: 'https://duckduckgo.com/'},
+    {name: 'ArchWiki', url: 'https://wiki.archlinux.org/index.php/'},
+    {name: 'Wikipedia', url: 'https://en.wikipedia.org/w/index.php'},
+    {name: 'Reddit', url: 'https://reddit.com/search/'},
+    {name: 'StackOverflow', url: 'https://stackoverflow.com/'}
 ];
 
-function chgAction() {
-    if (index < providers.length) {
-        index++;
-    } else {
-        index = 0;
-    }
-
+function chgAction() {    
     provider = providers[index];
-
     searchform.action = provider.url;
     input.placeholder = provider.name;
-
-    // switch (search) {
-    //     case 'ddg':
-
-    //         break;
-    //     default:
-    //         console.log(`Sorry, we are out of ${expr}.`);
-    // }
-
-    // var pos = GFG_Array.map(function(e) { 
-    //     return e.prop_2; 
-    // }).indexOf('val_32'); 
-
-    // if (search === 'ddg') {
-    //     search = 'google';
-    //     searchform.action = "https://www.google.com/search";
-    //     input.placeholder = 'Google';
-    // } else if (search === "google") {
-    //     search = 'github';
-    //     searchform.action = "https://github.com/";
-    //     input.placeholder = 'Github';
-    // } else if (search === 'github') {
-    //     search = 'gitlab';
-    //     searchform.action = "https://gitlab.com/";
-    //     input.placeholder = 'Gitlab';
-    // } else if (search === 'gitlab') {
-    //     search = 'arch';
-    //     searchform.action = 'https://wiki.archlinux.org/index.php/';
-    //     input.placeholder = 'ArchWiki';
-    // } else if (search === 'arch') {
-    //     search = 'wikipedia';
-    //     searchform.action = 'https://en.wikipedia.org/w/index.php';
-    //     input.placeholder = 'Wikipedia';
-    // } else if (search === 'wikipedia') {
-    //     search = 'reddit';
-    //     searchform.action = 'https://reddit.com/search/';
-    //     input.placeholder = 'Reddit';
-    // } else {
-    //     search = 'ddg';
-    //     searchform.action = 'https://duckduckgo.com/';
-    //     input.placeholder = 'DuckDuckGo';
-    // }
 
     if (provider.name === 'Wikipedia' || provider.name === 'ArchWiki') {
         input.setAttribute('name', 'search');
     } else {
         input.setAttribute('name', 'q');
+    }
+
+    if (index < providers.length) {
+        index++;
+    } else {
+        index = 0;
     }
 }
 
