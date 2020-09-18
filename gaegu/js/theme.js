3566ecc380f8b20d5af47d3205df945846c00b0e
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-09-18 07:40:01 -0400

    Autocommit por GitHub: alterações: add 'gaegu/index.html' add 'gaegu/js/theme.js'

--- gaegu/js/theme.js
+++ gaegu/js/theme.js
@@ -1,23 +1,27 @@
 const currentTheme = localStorage.getItem('theme');
 const logo = document.getElementById("logo");
 const icon = document.getElementById("icon");
+let theme = 'light';
 
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
   
     if (currentTheme === 'dark') {
+        theme = 'dark';
         logo.classList.add("invert");
         icon.src = "img/sun.svg";
     }
 }
 
 function switchTheme() {
-    if (currentTheme === 'light') {
+    if (theme === 'light') {
+        theme = 'dark';
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
         logo.classList.add("invert");
         icon.src = "img/sun.svg";
     } else {
+        theme = 'light';
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light');
         logo.classList.remove("invert");
