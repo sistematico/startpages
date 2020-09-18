9feeccf24d7db050fc1632c25988751c7cfe08e8
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-09-18 07:36:01 -0400

    Autocommit por GitHub: alterações: add 'gaegu/js/theme.js'

--- gaegu/js/theme.js
+++ gaegu/js/theme.js
@@ -1,4 +1,3 @@
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 const currentTheme = localStorage.getItem('theme');
 const logo = document.getElementById("logo");
 const icon = document.getElementById("icon");
@@ -7,14 +6,13 @@ if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
   
     if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
         logo.classList.add("invert");
         icon.src = "img/sun.svg";
     }
 }
 
 function switchTheme(e) {
-    if (e.target.checked) {
+    if (currentTheme === 'light') {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
         logo.classList.add("invert");
