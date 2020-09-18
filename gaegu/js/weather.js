be5f577888a2de4da7957fe379e043462eb0c285
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-09-18 07:31:01 -0400

    Autocommit por GitHub: alterações: add 'gaegu/js/weather.js'

--- gaegu/js/weather.js
+++ gaegu/js/weather.js
@@ -9,12 +9,10 @@ function getLocation() {
 }
 
 function showPosition(position) {
-
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
 
   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=3dedab9a1e2b6e1cb120ae2f421b477d`).then(function (response) {
-    //console.log(response)
     return response.json();
   }).then(function (json) {
     document.getElementById('weather').innerHTML = json.main.temp + '°C';
