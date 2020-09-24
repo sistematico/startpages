210cdc983138bd606432b62c3a7ef0e57ffd579e
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-09-24 18:13:01 -0400

    Autocommit por GitHub: alterações: add 'gaegu/edit.php'

--- gaegu/edit.php
+++ gaegu/edit.php
@@ -32,41 +32,12 @@
             </main>
 
             <footer>
-                <div class="cat">
-                    <ul class="devel">
-                        <li><h4>devel</h4></li>
-                        <li><a class="effect" href="https://localhost">localhost</a></li>
-                        <li><a class="effect" href="https://stackoverflow.com/">stackoverflow</a></li>
-                    </ul>
-                </div>
-                <div class="cat">
-                    <ul>
-                        <li><h4>info</h4></li>
-                        <li><a class="effect" href="https://developer.mozilla.org/en-US/">mdn</a></li>
-                        <li><a class="effect" href="https://www.taniarascia.com/how-to-use-json-data-with-php-or-javascript/">json</a></li>
-                    </ul>
-                </div>
-                <div class="cat">
-                    <ul>
-                        <li><h4>video</h4></li>
-                        <li><a class="effect" href="https://youtube.com">youtube</a></li>
-                        <li><a class="effect" href="https://dailymotion.com">dailymotion</a></li>
-                    </ul>
-                </div>
-                <div class="cat">
-                    <ul>
-                        <li><h4>social</h4></li>
-                        <li><a class="effect" href="https://facebook.com">facebook</a></li>
-                        <li><a class="effect" href="https://reddit.com">reddit</a></li>
-                    </ul>
-                </div>
             </footer>
         </div>
     </div>
     <script src="js/links.js"></script>
     <script src="js/theme.js"></script>
     <script src="js/weather.js"></script>
-    <script src="js/search.js"></script>
     <script src="js/time.js"></script>
 </body>
 </html>
