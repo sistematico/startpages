66c18ae3637479c80ec13f7f70a922adddc4ad06
Lucas Saliés Brum <sistematico@gmail.com>
2022-08-03 15:44:05 -0400

    Created mintab submenus

---
+++ mintab/assets/config.js
@@ -0,0 +1,50 @@
+const CONFIG = {
+  hour12  : false
+}
+
+const ICONS = [
+  {
+    icon: "ri-google-fill",
+    link: "https://www.google.com.br"
+  },
+  {
+    icon: "ri-youtube-fill",
+    link: "https://youtube.com"
+  },
+  {
+    icon: "ri-whatsapp-fill",
+    link: "https://web.whatsapp.com"
+  },
+  {
+    icon: "ri-reddit-fill",
+    link: "https://reddit.com"
+  },
+  {
+    icon: "ri-github-fill",
+    link: "https://github.com"
+  }
+]
+
+const LIST_ITEMS = [
+  {
+    name: "laravel",
+    link: "https://laravel.com/docs/9.x"
+  },
+  {
+    name: "vuejs",
+    link: "https://vuejs.org/guide/introduction.html"
+  },
+  {
+    name: "vitejs",
+    link: "https://vitejs.dev/guide"
+  },
+  {
+    name: "typescript",
+    link: "https://www.typescriptlang.org/docs/handbook/intro.html",
+    popover: [
+      { name: "Google", link: "https://google.com" },
+      { name: "Facebook", link: "https://facebook.com" },
+      { name: "Twitter", link: "https://twitter.com" }
+    ]
+  }
+]
---
+++ mintab/assets/js/setLinks.js
@@ -0,0 +1,54 @@
+const printIcons = () => {
+  for (const card of ICONS) {
+    let currentItem = document.createElement("a");
+    let currentItemIcon = document.createElement("span");
+
+    currentItem.classList.add("icon");
+    currentItem.href = card.link;
+
+    currentItemIcon.classList.add("fas");
+    currentItemIcon.classList.add(card.icon);
+
+    currentItem.append(currentItemIcon);
+    iconsContainer.appendChild(currentItem);
+  }
+};
+
+const printListItems = () => {
+  let currentItem = null;
+
+  for (const listItem of LIST_ITEMS) {
+    if (typeof listItem.popover === 'object') {
+      currentItem = document.createElement("details");
+      currentItem.dataset.popover = "up";
+      
+      let summary = document.createElement("summary");      
+      let div = document.createElement("div");
+      
+      for (const key in listItem.popover) {
+        let p = document.createElement("p");
+        let link = document.createElement("a");
+
+        link.href = listItem['popover'][key].link;
+        link.appendChild(document.createTextNode(listItem['popover'][key].name));
+
+        p.appendChild(link);
+        div.appendChild(p);
+      }
+        
+      summary.appendChild(document.createTextNode(listItem.name));
+      currentItem.appendChild(summary);
+      currentItem.appendChild(div);
+    } else {
+      currentItem = document.createElement("a");
+      currentItem.href = listItem.link;
+      currentItem.appendChild(document.createTextNode(listItem.name));      
+    }
+    
+    currentItem.classList.add("listItem");
+    listContainer.appendChild(currentItem);
+  }
+};
+
+printIcons();
+printListItems();
